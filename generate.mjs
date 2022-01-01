38d6761799a4235c9e57db6b7d2fef3dadfdbe62
Siddharth <ahoy@barelyhuman.dev>
2022-01-01 14:14:55 +0530

    v1.1.0 build

---
+++ generate.mjs
@@ -0,0 +1,58 @@
+import { colorsByVariant } from "@rose-pine/palette";
+import prettier from "prettier";
+import fs from "fs";
+import { paramCase } from "param-case";
+
+const TEMPLATE_KEY = "{{template}}";
+const STYLE_VARIABLE_PREFIX = "rose-";
+const styleSheetTemplate = `
+    :root{
+        ${TEMPLATE_KEY}
+    }
+`;
+
+let styleSheet = ``;
+
+const pathToCSSVariable = (path) => {
+  return `--${path}`;
+};
+
+const varToStyleString = (variable, value) => {
+  return `${variable}:${value};`;
+};
+
+const appendToStylesheet = (cssString) => {
+  if (!styleSheet) {
+    styleSheet = styleSheetTemplate;
+  }
+  return styleSheet.replace(
+    new RegExp(TEMPLATE_KEY),
+    `${cssString}\n{{template}}`
+  );
+};
+
+const purgeTemplate = (template) => {
+  return template.replace(new RegExp(TEMPLATE_KEY), "");
+};
+
+function main() {
+  const variants = Object.keys(colorsByVariant);
+  variants.forEach((variant) => {
+    let path = `${STYLE_VARIABLE_PREFIX}${variant}-`;
+    Object.keys(colorsByVariant[variant]).forEach((colorRole) => {
+      const colorRoleText = paramCase(colorRole);
+      const localPath = `${path}${colorRoleText}`;
+      const color = colorsByVariant[variant][colorRole];
+      const cssVarName = pathToCSSVariable(localPath);
+      const cssVarString = varToStyleString(cssVarName, color);
+      styleSheet = appendToStylesheet(cssVarString);
+    });
+  });
+
+  styleSheet = purgeTemplate(styleSheet);
+  const cleanedStyles = prettier.format(styleSheet, { parser: "css" });
+
+  fs.writeFileSync("dist/rose-pine.css", cleanedStyles);
+}
+
+main();
---
+++ readme.md
@@ -0,0 +1,36 @@
+# rose-pine-css
+
+> Soho vibes from [rose-pine](https://github.com/rose-pine/rose-pine-theme), as css variables
+
+This is not from the offical devs which I wish find time to get this done in the official package, but till then, I made one for my own use
+
+## Why?
+
+I use css and css variables more than css modules or inline styles so, this is kinda a needed in most of my web projects, considering I like the color pallete quite a bit.
+
+## Usage
+
+It's a css package, there's no other imports
+
+- You can use a npm cdn to get the css file
+
+```
+<link rel="stylesheet" href="https://unpkg.com/@barelyhuman/rose-pine-css@0.0.1/rose-pine.css">
+```
+
+- or you can install the package and import the css (your bundler needs to support css imports)
+
+```
+import "@barelyhuman/rose-pine-css/rose-pine.css"
+```
+
+## Credits
+
+For the original project by
+
+- [mvllow](https://github.com/mvllow)
+- [fvrests](https://github.com/fvrests)
+
+## License
+
+[MIT](/LICENSE)
